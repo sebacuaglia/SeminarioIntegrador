2683df33760b4650cf5f78ac46eee44c7398e3cf
sebastian <sebastian@Lenovo>
2024-08-22 21:30:44 -0300

    Inicio proyecto

---
+++ Backend/findpets/src/main/java/ar/com/findpets/FindpetsApplication.java
@@ -0,0 +1,13 @@
+package ar.com.findpets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindpetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindpetsApplication.class, args);
+	}
+
+}
